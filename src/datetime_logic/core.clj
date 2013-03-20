c59b82edd3a344c47aa8dd46e5ef51d3c7d0197a
Stephen Sloan <steve@stevesloan.com>
2013-03-19 17:47:25 -0700

    Comparing date-times

--- src/datetime_logic/core.clj
+++ src/datetime_logic/core.clj
@@ -3,7 +3,8 @@
   (:use
     clojure.core.logic)
   (:require
-    [clj-time.core :as time]))
+    [clj-time.core :as time]
+    [clojure.core.logic.fd :as fd]))
 
 ; The following is a modification of the extending core.logic with Datomic example
 ; https://github.com/clojure/core.logic/wiki/Extending-core.logic-%28Datomic-example%29
@@ -25,6 +26,128 @@
   (unify-terms [u v s]
     (unify-with-datetime* u v s)))
 
-; Unifying against a DateTime requires that the DateTime be on the left side.
+; Unifying against a DateTime requires that the DateTime be on the left side. Normally == shouldn't care about variable
+; order.  Using datetimeo instead of == when one unifying a DateTime with a collection of lvars seems safer to me.
 (defn datetimeo [date-time & vars]
   (== date-time vars))
+
+(defn gt-yearo
+  [dt-1 dt-2 out]
+  (fresh [year-1 year-2]
+    (datetimeo dt-1 year-1)
+    (datetimeo dt-2 year-2)
+    (fd/> year-1 year-2)
+    (== dt-1 out)))
+
+(defn eq-yearo
+  [dt-1 dt-2 out]
+  (fresh [year-1 year-2]
+    (datetimeo dt-1 year-1)
+    (datetimeo dt-2 year-2)
+    (== year-1 year-2)
+    (== dt-1 out)))
+
+(defn gt-montho
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 year-2 month-2]
+    (datetimeo dt-1 year-1 month-1)
+    (datetimeo dt-2 year-2 month-2)
+    (conde
+      [(eq-yearo dt-1 dt-2 dt-1) (fd/> month-1 month-2)]
+      [(gt-yearo dt-1 dt-2 dt-1)])
+    (== dt-1 out)))
+
+(defn eq-montho
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 year-2 month-2]
+    (eq-yearo dt-1 dt-2 dt-1)
+    (datetimeo dt-1 year-1 month-1)
+    (datetimeo dt-2 year-2 month-2)
+    (== month-1 month-2)
+    (== out dt-1)))
+
+(defn gt-dateo
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 year-2 month-2 date-2]
+    (datetimeo dt-1 year-1 month-1 date-1)
+    (datetimeo dt-2 year-2 month-2 date-2)
+    (conde
+      [(eq-montho dt-1 dt-2 dt-1) (fd/> date-1 date-2)]
+      [(gt-montho dt-1 dt-2 dt-1)])
+    (== dt-1 out)))
+
+(defn eq-dateo
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 year-2 month-2 date-2]
+    (eq-montho dt-1 dt-2 dt-1)
+    (datetimeo dt-1 year-1 month-1 date-1)
+    (datetimeo dt-2 year-2 month-2 date-2)
+    (== date-1 date-2)
+    (== out dt-1)))
+
+(defn gt-houro
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 year-2 month-2 date-2 hour-2]
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2)
+    (conde
+      [(eq-dateo dt-1 dt-2 dt-1) (fd/> hour-1 hour-2)]
+      [(gt-dateo dt-1 dt-2 dt-1)])
+    (== dt-1 out)))
+
+(defn eq-houro
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 year-2 month-2 date-2 hour-2]
+    (eq-dateo dt-1 dt-2 dt-1)
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2)
+    (== hour-1 hour-2)
+    (== out dt-1)))
+
+(defn gt-minuteo
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 minute-1 year-2 month-2 date-2 hour-2 minute-2]
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1 minute-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2 minute-2)
+    (conde
+      [(eq-houro dt-1 dt-2 dt-1) (fd/> minute-1 minute-2)]
+      [(gt-houro dt-1 dt-2 dt-1)])
+    (== dt-1 out)))
+
+(defn eq-minuteo
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 minute-1 year-2 month-2 date-2 hour-2 minute-2]
+    (eq-houro dt-1 dt-2 dt-1)
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1 minute-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2 minute-2)
+    (== minute-1 minute-2)
+    (== out dt-1)))
+
+(defn gt-secondo
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 minute-1 second-1 year-2 month-2 date-2 hour-2 minute-2 second-2]
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1 minute-1 second-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2 minute-2 second-2)
+    (conde
+      [(eq-minuteo dt-1 dt-2 dt-1) (fd/> second-1 second-2)]
+      [(gt-minuteo dt-1 dt-2 dt-1)])
+    (== dt-1 out)))
+
+(defn eq-secondo
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 minute-1 second-1 year-2 month-2 date-2 hour-2 minute-2 second-2]
+    (eq-minuteo dt-1 dt-2 dt-1)
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1 minute-1 second-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2 minute-2 second-2)
+    (== second-1 second-2)
+    (== out dt-1)))
+
+(defn gt-milliseco
+  [dt-1 dt-2 out]
+  (fresh [year-1 month-1 date-1 hour-1 minute-1 second-1 millisec-1 year-2 month-2 date-2 hour-2 minute-2 second-2 millisec-2]
+    (datetimeo dt-1 year-1 month-1 date-1 hour-1 minute-1 second-1 millisec-1)
+    (datetimeo dt-2 year-2 month-2 date-2 hour-2 minute-2 second-2 millisec-2)
+    (conde
+      [(eq-secondo dt-1 dt-2 dt-1) (fd/> millisec-1 millisec-2)]
+      [(gt-secondo dt-1 dt-2 dt-1)])
+    (== dt-1 out)))
--- test/datetime_logic/core_test.clj
+++ test/datetime_logic/core_test.clj
@@ -32,3 +32,204 @@
            (run* [year month day hour minute second millisecond]
              (datetimeo dt year month day hour minute second millisecond)))))
 )
+
+(deftest gt-yearo-test
+  (let [dt-1 (t/date-time 2012 1 1 12 0 0)
+        dt-2 (t/date-time 2013 1 1 12 0 0)]
+    (is
+      (=
+        [dt-2]
+
+        (run* [out]
+          (gt-yearo dt-2 dt-1 out))))
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-yearo dt-1 dt-2 out))))
+
+    (is
+      (=
+        [true]
+
+        (run* [q]
+          (gt-yearo dt-2 dt-1 dt-2) (== q true))))
+  )
+)
+
+(deftest gt-montho-test
+  (let [dt-1 (t/date-time 2012 1 1 12 0 0)
+        dt-2 (t/date-time 2013 1 1 12 0 0)
+        dt-3 (t/date-time 2013 2 1 12 0 0)]
+
+    (is
+      (=
+        [dt-3]
+
+        (run* [out]
+          (gt-montho dt-3 dt-2 out))))
+
+    (is
+      (=
+        [dt-2]
+
+        (run* [out]
+          (gt-montho dt-2 dt-1 out))))
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-montho dt-1 dt-2 out))))
+  )
+)
+
+(deftest gt-dateo-test
+  (let [dt-1 (t/date-time 2012 1 1 12 0 0)
+        dt-2 (t/date-time 2013 1 1 12 0 0)
+        dt-3 (t/date-time 2013 2 1 12 0 0)
+        dt-4 (t/date-time 2013 2 2 12 0 0)]
+
+    (is
+      (=
+        [dt-4]
+
+        (run* [out]
+          (gt-dateo dt-4 dt-3 out))))
+
+    (is
+      (=
+        [dt-3]
+
+        (run* [out]
+          (gt-dateo dt-3 dt-2 out))))
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-dateo dt-1 dt-2 out))))
+  )
+)
+
+(deftest gt-houro-test
+  (let [dt-3 (t/date-time 2013 2 1 12 0 0)
+        dt-4 (t/date-time 2013 2 2 12 0 0)
+        dt-5 (t/date-time 2013 2 2 13 0 0)]
+
+    (is
+      (=
+        [dt-5]
+
+        (run* [out]
+          (gt-houro dt-5 dt-4 out))))
+
+    (is
+      (=
+        [dt-4]
+
+        (run* [out]
+          (gt-houro dt-4 dt-3 out))))
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-houro dt-4 dt-5 out))))
+  )
+)
+
+(deftest gt-minuteo-test
+  (let [dt-6 (t/date-time 2013 2 2 13 10 0)
+        dt-7 (t/date-time 2013 2 2 14 10 0)
+        dt-8 (t/date-time 2013 2 2 14 20 0)]
+
+    (is
+      (=
+        [dt-8]
+
+        (run* [out]
+          (gt-minuteo dt-8 dt-7 out))))
+
+    (is
+      (=
+        [dt-7]
+
+        (run* [out]
+          (gt-minuteo dt-7 dt-6 out))))
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-minuteo dt-7 dt-8 out))))
+  )
+)
+
+(deftest gt-secondo-test
+  (let [dt-9  (t/date-time 2013 2 2 14 20 5)
+        dt-10 (t/date-time 2013 2 2 14 30 5)
+        dt-11 (t/date-time 2013 2 2 14 30 10)]
+
+    (is
+      (=
+        [dt-11]
+
+        (run* [out]
+          (gt-secondo dt-11 dt-10 out))))
+
+    (is
+      (=
+        [dt-10]
+
+        (run* [out]
+          (gt-secondo dt-10 dt-9 out))))
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-secondo dt-10 dt-11 out))))
+  )
+)
+
+(deftest gt-milliseco-test
+  (let [dt-12 (t/date-time 2013 2 2 14 30 10 5)
+        dt-13 (t/date-time 2013 2 2 14 30 20 5)
+        dt-14 (t/date-time 2013 2 2 14 30 20 10)]
+
+    (is
+      (=
+        [dt-14]
+
+        (run* [out]
+          (gt-milliseco dt-14 dt-13 out))
+      )
+    )
+
+    (is
+      (=
+        [dt-13]
+
+        (run* [out]
+          (gt-milliseco dt-13 dt-12 out))
+      )
+    )
+
+    (is
+      (=
+        []
+
+        (run* [out]
+          (gt-milliseco dt-13 dt-14 out))
+      )
+    )
+  )
+)
