5b8ec99bcc1553ee5962def318c298ad54514285
Stephen Sloan <steve@stevesloan.com>
2013-03-20 15:26:06 -0700

    updating project.clj

--- project.clj
+++ project.clj
@@ -6,5 +6,4 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [clj-time "0.4.3"]
                  [org.clojure/core.logic "0.8.0-rc2"]]
-  :user {:plugins [[lein-clojars "0.9.1"]]}
 )
