d0c0f6aab9efac7c25cef6f842c9236783b3803f
Stephen Sloan <steve@stevesloan.com>
2014-02-26 23:11:36 -0800

    Another version of the eg test macro

--- test/calendar_logic/test/test_helper.clj
+++ test/calendar_logic/test/test_helper.clj
@@ -1,5 +1,8 @@
 (ns calendar-logic.test.test-helper
   (:use clojure.test))
 
-(defmacro eg [actual _ expectation]
-  `(is (= ~expectation ~actual)))
+(defmacro eg
+  ([actual _ expectation]
+     `(is (= ~expectation ~actual)))
+  ([actual expectation]
+     `(is (= ~expectation ~actual))))
